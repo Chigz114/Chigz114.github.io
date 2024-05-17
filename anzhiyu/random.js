99738fb1f10b09cc85e7474ac2e893d62c2cbe25
Chigz114 <1489947691@qq.com>
2024-05-18 00:29:13 +0800

    Site updated: 2024-05-18 00:29:13

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/05/17/hello-world/","2024/05/18/ECE220-MP11/"];function toRandomPost(){
+var posts=["2024/05/18/ECE220-MP11/","2024/05/17/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
