da7261ed1e53e0a8e174343a56b528716fea2f52
Chigz114 <1489947691@qq.com>
2024-05-20 00:29:22 +0800

    Site updated: 2024-05-20 00:29:21

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/05/18/ECE220-MP11/","2024/05/17/hello-world/","2024/05/18/SuiBi/","2024/05/19/Dynamic Resizing/","2024/05/19/Fully Dynamic Allocation/"];function toRandomPost(){
+var posts=["2024/05/18/ECE220-MP11/","2024/05/17/hello-world/","2024/05/18/SuiBi/","2024/05/19/Dynamic Resizing/","2024/05/19/Fully Dynamic Allocation/","2024/05/19/Cpp/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
