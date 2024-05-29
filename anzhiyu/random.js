2b0413c383fb3a9cb2f33dc87ef878af298835d4
Chigz114 <1489947691@qq.com>
2024-05-29 14:04:11 +0800

    Site updated: 2024-05-29 14:04:11

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/05/18/ECE220-MP11/","2024/05/17/hello-world/","2024/05/18/SuiBi/","2024/05/19/Dynamic Resizing/","2024/05/19/Fully Dynamic Allocation/","2024/05/19/Cpp/","2024/05/21/Containers-and-Iterators/","2024/05/21/Function-Pointers-and-Callbacks/","2024/05/22/IO-in-C/","2024/05/23/three-part-IO/"];function toRandomPost(){
+var posts=["2024/05/18/SuiBi/","2024/05/19/Dynamic Resizing/","2024/05/19/Cpp/","2024/05/21/Containers-and-Iterators/","2024/05/21/Function-Pointers-and-Callbacks/","2024/05/22/IO-in-C/","2024/05/23/three-part-IO/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
