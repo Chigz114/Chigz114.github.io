884534387fcca2e5a1a68f48e910b9965743776f
userName <userEmail>
2024-09-19 15:38:21 +0800

    Site updated: 2024-09-19 15:38:21

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/09/09/任务一/","2024/09/09/第一次例会/","2024/09/09/重要通知/"];function toRandomPost(){
+var posts=["2024/09/09/任务一/","2024/09/09/第一次例会/","2024/09/09/重要通知/","2024/09/19/任务二/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
